16d0da1f3800ec98870ed672f64091b322a8a619
chebroluabhiram123456-coder <chebroluabhiram123456@gmail.com>
2025-09-01 21:36:35 +0530

    Update settings.gradle.kts

--- android/settings.gradle.kts
+++ android/settings.gradle.kts
@@ -5,9 +5,9 @@ pluginManagement {
         gradlePluginPortal()
     }
     plugins {
-        id("com.android.application") version "8.7.0"
-        id("com.android.library") version "8.7.0"
-        id("org.jetbrains.kotlin.android") version "2.0.20"
+        id("com.android.application") version "8.6.0"
+        id("com.android.library") version "8.6.0"
+        id("org.jetbrains.kotlin.android") version "1.9.24"
     }
 }
 
