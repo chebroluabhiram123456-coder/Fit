c008768b1e35748b022d9e61239f70e9ee9a935a
chebroluabhiram123456-coder <chebroluabhiram123456@gmail.com>
2025-09-01 21:47:42 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,4 +1,8 @@
+// Top-level build file
+
 plugins {
-    id("com.android.application") version "8.6.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.android.application") version "8.7.0" apply false
+    id("com.android.library") version "8.7.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.20" apply false
+    id("dev.flutter.flutter-gradle-plugin") version "1.0.0" apply false
 }
