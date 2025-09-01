adfbb5cecdddd10eb6631233af885ce896efde5d
chebroluabhiram123456-coder <chebroluabhiram123456@gmail.com>
2025-09-01 14:49:27 +0530

    Create build.gradle.kts

---
+++ android/build.gradle.kts
@@ -0,0 +1,36 @@
+plugins {
+    id("com.android.application")
+    id("kotlin-android")
+    id("dev.flutter.flutter-gradle-plugin")
+}
+
+android {
+    namespace = "com.example.fit_tracker"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.fit_tracker"
+        minSdk = 21
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+}
+
+flutter {
+    source = "../.."
+}
+
+dependencies {
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk7:1.9.24")
+}
