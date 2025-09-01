05b4b7cadd5b7ce3f30e7c5bbaf7217e40c5394b
chebroluabhiram123456-coder <chebroluabhiram123456@gmail.com>
2025-09-01 21:09:04 +0530

    Update build.gradle.kts

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -1,12 +1,12 @@
+// Top-level build.gradle.kts
+
 plugins {
+    // No application plugin here, only dependencies for build system
     id("com.android.application") version "8.3.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.24" apply false
     id("dev.flutter.flutter-gradle-plugin") apply false
 }
 
-allprojects {
-    repositories {
-        google()
-        mavenCentral()
-    }
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
